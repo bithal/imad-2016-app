947024e36e8657a7f0ce11aed914384416365b6e
bithal <anuranjan.panigrahi@gmail.com>
2016-10-01 11:29:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,12 +6,13 @@ element.innerHTML="New Value";
 
 //make image move
 var img = document.getElementById("madi");
-//var marginleft = 0;
-// function moveRight(){
-//     marginleft = marginleft+10;
-//     img.style.marginLeft=marginLeft+'px';
-}
 img.onclick=function () {
  // var interval = setInterval(moveRight,100);
  img.style.marginLeft='100px';
 };
+
+//var marginleft = 0;
+// function moveRight(){
+//     marginleft = marginleft+10;
+//     img.style.marginLeft=marginLeft+'px';
+
